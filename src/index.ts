e59b4e49c9051c7c863568431eedcf27f6f2408d
noname0310 <hjnam2014@gmail.com>
2024-09-23 00:38:21 +0900

    reproduce new problem

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,8 @@ await new Promise(resolve => window.onload = resolve);
 const canvas = document.createElement("canvas");
 const engine = new Engine(canvas);
 const scene = new Scene(engine);
-new DefaultRenderingPipeline("default", true, scene);
+const defaultPipeline = new DefaultRenderingPipeline("default", true, scene);
+defaultPipeline.prepare(); // uncomment this line to fix the issue
 
 scene.dispose();
 engine.dispose();
